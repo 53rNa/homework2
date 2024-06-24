06d7514e3cfa515418fa68631c22984162600ace
53rNa <53rNa76@gmail.com>
2024-06-24 23:20:12 +0300

    Add files via upload
    Решение к практическому заданию по работе в Pycharm - "Переменные"

---
+++ homework2.py
@@ -0,0 +1,8 @@
+numbers_of_HW = 12
+numbers_of_hours = 1.5
+course_name = 'Python'
+time_per_task = (numbers_of_hours / numbers_of_HW)
+print( f'Курс: {course_name},',
+       f'всего задач: {numbers_of_HW},',
+       f'затрачено часов: {numbers_of_hours},',
+       f'среднее время выполнения: {time_per_task}','часа.' )
